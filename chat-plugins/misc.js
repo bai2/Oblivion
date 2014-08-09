54ccfade2dfc97d7225852a6e2524a05cdd96383
小太 <nospam@xiaotai.org>
2014-08-09 16:26:58 +1000

    Allow /fb to be broadcast

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -1,5 +1,6 @@
 exports.commands = {
 	fb: function () {
+		if (!this.canBroadcast()) return;
 		this.sendReplyBox("<strong>se están buscando batallas en el ladder:</strong>: " + Tools.escapeHTML(Object.keys(Rooms.rooms.global.searchers.reduce(function (prev, search) {
 			prev[Tools.getFormat(search.formatid).name] = 1;
 			return prev;
