aecf19f7cec73a836c56fff1a29ba6890bae7479
小太 <nospam@xiaotai.org>
2014-08-09 16:23:12 +1000

    Translate and style /fb

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -1,8 +1,8 @@
 exports.commands = {
 	fb: function () {
-		this.sendReply("Ladder searchers: " + Object.keys(Rooms.rooms.global.searchers.reduce(function (prev, search) {
+		this.sendReplyBox("<strong>se están buscando batallas en el ladder:</strong>: " + Tools.escapeHTML(Object.keys(Rooms.rooms.global.searchers.reduce(function (prev, search) {
 			prev[Tools.getFormat(search.formatid).name] = 1;
 			return prev;
-		}, {})).join(", "));
+		}, {})).join(", ")));
 	}
 };
