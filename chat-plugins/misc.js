4035c0ddb9475dcce543a9060d85269bacb416e6
BasedGod7 <hummer1491@hotmail.com>
2014-08-09 22:17:47 -0700

    Update misc.js

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -1,7 +1,7 @@
 exports.commands = {
 	fb: function () {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox("<strong>se están buscando batallas en el ladder:</strong>: " + Tools.escapeHTML(Object.keys(Rooms.rooms.global.searchers.reduce(function (prev, search) {
+		this.sendReplyBox("<strong>Se están buscando batallas en el ladder:</strong>: " + Tools.escapeHTML(Object.keys(Rooms.rooms.global.searchers.reduce(function (prev, search) {
 			prev[Tools.getFormat(search.formatid).name] = 1;
 			return prev;
 		}, {})).join(", ")));
